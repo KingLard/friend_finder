0110491161727910ba8a780d6a1cfc0f41fc978b
Brett Landes <brettl0407@gmail.com>
2020-05-03 12:44:21 -0400

    changed port to have process.env in it to prepare to push to heroku

--- server.js
+++ server.js
@@ -4,13 +4,12 @@
 var express = require("express");
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));  
 
 
 
